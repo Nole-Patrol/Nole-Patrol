03a037e31ed664fcebad416255497dcefcf0ea30
caitlin <caitlinmariegrimes@users.noreply.github.com>
2023-10-27 16:38:31 -0400

    Configure efficient data imports, implement search functionality, update documentation
    + Configure updatemodels.py for efficient database inserts using bulk_create(); update contents of source field to contain breach site
    + Update models.py to include encryption for stored passwords
    + Configure views.py to render search.html with correct results
    + Configure search.html to display results
    + Draft README.md and update documentation in code files
    
    Co-Authored-By: NaturePA <68764086+NaturePA@users.noreply.github.com>

--- np_app/views.py
+++ np_app/views.py
@@ -1,10 +1,57 @@
+'''
+Description: This file contains the code for the views of the np_app.
+Author(s): Michael Sousa, Brian Arango, Caitlin Marie Grimes
+Last Modified Date: 27 October 2023
+Assumptions: N/A
+References: https://docs.djangoproject.com/en/4.2/topics/http/views/
+            https://docs.djangoproject.com/en/4.2/ref/request-response/
+'''
 import os
 from django.shortcuts import render
 from .forms import EmailSearchForm
 from .models import EmailFile
 
+'''
+Function Name: index(request)
+Description: This function contains the code to render the index.html page and
+             process the user's email search query.
+Parameters: request
+Return Value: HTTPResponse
+Author(s): Michael Sousa Jr., Caitlin Marie Grimes
+Last Modified Date: 27 October 2023
+Assumptions: N/A
+References: https://docs.djangoproject.com/en/4.2/ref/request-response/
+'''
 def index(request):
-    return render(request, 'index.html')
+    print("Request method:", request.method)  # Debugging statement
+    
+    if request.method == 'POST': 
+        form = EmailSearchForm(request.POST)
+        
+        if form.is_valid():
+            user_email = form.cleaned_data['email']
+            print("User email:", user_email)  # Debugging statement
+
+            # Use Django's ORM to search for the email in the database.
+            matching_records = EmailFile.objects.filter(name=user_email)
+            print("Matching records:", matching_records)  # Debugging statement
+
+            # If the email is found in the database, extract the distinct sources.
+            if matching_records.exists():
+                sources = set(record.source for record in matching_records)
+                # Render the search.html page with the results.
+                return render(request, 'search.html', {'user_email': user_email, 'sources': sources})
+            else: 
+                print("No matching records for:", user_email)  # Debugging statement
+                # Render the search.html page with the no_match flag set to True.
+                return render(request, 'search.html', {'no_match': True})
+        else:
+            print("Form errors:", form.errors)  # Debugging statement
+
+    else: 
+        form = EmailSearchForm()
+    
+    return render(request, 'index.html', {'form': form})
             
 def breaches_page(request):
     return render(request, 'breaches.html')
