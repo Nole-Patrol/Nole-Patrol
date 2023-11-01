cdcfbd16ae49052bc3aba8051c9bfcf98df1cba9
caitlin <caitlinmariegrimes@users.noreply.github.com>
2023-11-01 14:21:19 -0400

    Add HIBP API key, format response.text
    + Add HIBP API key
    + Parse response.text according to EmailFile table schema to prepare for record insertion

--- np_app/apis.py
+++ np_app/apis.py
@@ -1,9 +1,10 @@
 import requests
 
+account = 'cgrim.wa@gmail.com'
 def get_pwned(account):
 
     url = "https://haveibeenpwned.com/api/v3/breachedaccount/" + account
-    hibp_api_key = '<key>'
+    hibp_api_key = '71113c7ccb05453fbeb9d79b1121b2ef'
     payload={}
     headers = {
         'hibp-api-key': str(hibp_api_key),
@@ -11,7 +12,21 @@ def get_pwned(account):
         'timeout': '2.5',
         'HIBP': str(hibp_api_key),
     }
-
+    print(url)
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
+    #print(response.text)
+
+    response_string = response.text
+    print(response_string)
+    for i in response_string:
+        response_string = response_string.replace(
+            '"', "").replace(
+                '[', "").replace(']', "").replace(
+                    '{', "").replace(
+                        '}', "").replace(
+                            'Name:', account+":NULL:")
+    print(response_string)
+
+if __name__ == '__main__':
+    get_pwned(account)
